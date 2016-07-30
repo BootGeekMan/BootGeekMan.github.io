e179b73384e8f8d4d5ee18e1932939c0fc369372
BootGeekMan <bootlineinc@gmail.com>
2016-07-30 18:42:17 +0300

    asdfadsf
    asdfasdf

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ function GoHowTo() {
 				var target= $('#HowTo');
 				$('html, body').stop().animate({
 				   scrollTop: target.offset().top
-				}, 500);
+				}, 300);
 			  }
 function GoUp() {
 	var articlePadding = 150;
 	var target= $('.header');
 	$('html, body').stop().animate({
 	   scrollTop: target.offset().top - articlePadding
-	}, 500);
+	}, 300);
   }		
